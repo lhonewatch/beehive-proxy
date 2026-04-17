cd6649dadb4a463546fa54b3621f1e7027613c40
Leo Honewatch <lhonewatch@users.noreply.github.com>
2026-04-17 18:26:36 +0800

    docs: add comment to BodyTransform config integration init

--- internal/config/config_bodytransform_integration.go
+++ internal/config/config_bodytransform_integration.go
@@ -1,5 +1,7 @@
 package config
 
+// init registers the BodyTransform configuration extension, which populates
+// the BodyTransform field from environment variables during config loading.
 func init() {
 	registerExtension(func(c *Config) error {
 		c.BodyTransform = bodyTransformConfigFromEnv()
